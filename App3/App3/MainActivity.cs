d4cdb77c0d76b4f9595e10512020db230bdb9f6c
Piotrekk85 <piotr.jastr@gmail.com>
2017-04-09 13:05:35 +0200

    Add files via upload

---
+++ App3/App3/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+
+namespace App3
+{
+    [Activity(Label = "App3", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : ListActivity
+    {
+        string[] items;
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            items = new string[] { "Vegetables", "Fruits", "Flower Buds", "Legumes", "Bulbs", "Tubers" };
+            ListAdapter = new ArrayAdapter<String>(this, Android.Resource.Layout.SimpleListItem1, items);
+        }
+    }
+}
+
